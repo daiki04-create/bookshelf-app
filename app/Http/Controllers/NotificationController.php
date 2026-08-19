73bd66a20fbed2a03b6a57ff1af4782223303ce6
daiki <xuvy73c6eqkrabbiw@icloud.com>
2026-08-19 19:24:33 +0900

    feat: 通知機能実装を完了

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -2,7 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
+
 class NotificationController extends Controller
 {
-    //
+    public function index(Request $request): View
+    {
+        $notifications = $request->user()->notifications()->latest()->paginate(10);
+
+        return view('notifications.index', compact('notifications'));
+    }
+
+    public function read(Request $request, string $id): RedirectResponse
+    {
+        $notification = $request->user()->notifications()->findOrFail($id);
+        $notification->markAsRead();
+
+        return back();
+    }
 }
